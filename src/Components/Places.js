1f18bbf3fe133fa9b0b5a634a2d015abe8270cc2
John Ramirez <jvramirez13@gmail.com>
2019-05-22 10:18:31 -0400

    Display Open Restaurants
    Added a feature that shows opened restaurants in Charlottesville!

--- src/Components/Places.js
+++ src/Components/Places.js
@@ -12,7 +12,6 @@ export default class Layout extends React.Component {
 
     componentDidMount(){
         axios.get("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.033554,-78.507980&radius=1500&type=restaurant&key=" + API_KEY).then(res => {
-        console.log(res.data.results)
         this.setState({
             locations: res.data.results
             });
@@ -25,12 +24,13 @@ export default class Layout extends React.Component {
             })
 
             let openRes = []
-
-            for(let i = 0; i < this.state.locations; i++){
+            for(let i = 0; i < this.state.locations.length; i++){
                 if(this.state.locations[i].opening_hours.open_now === true){
-                    openRes.push(this.state.locations[i])
+                    openRes.push(this.state.locations[i].name)
                 }
             }
+
+            console.log(openRes[0])
 
             this.setState({
                 open: openRes
@@ -49,10 +49,10 @@ export default class Layout extends React.Component {
                   <li key={resulter.id}>{resulter.name}</li>
               ))}
           </ul>
-          <h2>OPEN RESTAURANTS:</h2>
+          <h2 text-align = "left">OPEN RESTAURANTS:</h2>
           <ul>
               {this.state.open.map(opened => (
-                  <li key={opened.id}>{opened.name}</li>
+                  <h2>{opened}</h2>
               ))}
           </ul>
       </div>
